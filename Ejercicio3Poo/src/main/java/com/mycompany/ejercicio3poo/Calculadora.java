371c4ad1869e46ec9e3ce09eba95a4c3ec0b8a35
estebanv <esteb097@gmail.com>
2023-03-01 15:39:58 -0500

    correcion

--- Ejercicio3Poo/src/main/java/com/mycompany/ejercicio3poo/Calculadora.java
+++ Ejercicio3Poo/src/main/java/com/mycompany/ejercicio3poo/Calculadora.java
@@ -11,7 +11,7 @@ public class Calculadora {
         return resultadoSuma;
     }
 
-    public String restar(int numero1, int numero2){
+    public String restar(int numero1, int numero2) {
         int resultadoResta = numero1 - numero2;
         String restar = "el resultado de la resta es : " + resultadoResta;
         return restar;
@@ -22,23 +22,23 @@ public class Calculadora {
         return resultadoMultiplicacion;
     }
 
-    public float division(ModeloCalculadora dividir ){
-        float resultado = dividir.getNumeroUno()/dividir.getNumeroDos();
+    public float division(ModeloCalculadora dividir) {
+        float resultado = dividir.getNumeroUno() / dividir.getNumeroDos();
         return resultado;
     }
 
     public String potencia() {
         Random numeroAleatorio = new Random();
         int random = numeroAleatorio.nextInt(100);
-        int potencia = random ^ 2;
+        double potencia = Math.pow(random, 2);
         String resultadoPotencias = "el resultado de potencia aleatoria es : " + potencia;
         return resultadoPotencias;
     }
 
     public String raiz(String numero) {
-       
+
         int numEntero = Integer.parseInt(numero);
-        int raiz = (int)Math.sqrt(numEntero);
+        int raiz = (int) Math.sqrt(numEntero);
         String resultadoRaiz = "la raiz es : " + raiz;
         return resultadoRaiz;
     }
@@ -46,27 +46,26 @@ public class Calculadora {
     public Calculadora() {
 
         Random random = new Random();
-        ModeloCalculadora divisioncalculadora = new ModeloCalculadora();
-        divisioncalculadora.setNumeroDos(random.nextInt(100));
-        divisioncalculadora.setNumeroUno(random.nextInt(100));
-        System.out.println( "la sumas aleatoria es : "+sumar(random.nextInt(100),random.nextInt(100),random.nextInt(100)));
-        System.out.println("la resta aleatoria " +restar(random.nextInt(100),random.nextInt(100)));
-        System.out.println("la multipliacion aleatoria es "+multiplicar(random.nextInt(100),random.nextInt(100),random.nextInt(100),random.nextInt(100),random.nextInt(100)));
+        ModeloCalculadora modelCalculadora = new ModeloCalculadora();
+        modelCalculadora.setNumeroDos(random.nextInt(100));
+        modelCalculadora.setNumeroUno(random.nextInt(100));
+        System.out.println("la sumas aleatoria es : " + sumar(random.nextInt(100), random.nextInt(100), random.nextInt(100)));
+        System.out.println("la resta aleatoria " + restar(random.nextInt(100), random.nextInt(100)));
+        System.out.println("la multipliacion aleatoria es " + multiplicar(random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100), random.nextInt(100)));
         System.out.println(potencia());
-        System.out.println(raiz (String.valueOf(random.nextInt(100))));
-        System.out.println("la division aleatoria es :"+division(divisioncalculadora));
+        System.out.println(raiz(String.valueOf(random.nextInt(100))));
+        System.out.println("la division aleatoria es :" + division(modelCalculadora));
     }
 
-    
-    public Calculadora(int num1, int num2, int num3, int num4, int num5){
-       
+    public Calculadora(int num1, int num2, int num3, int num4, int num5) {
+
         this.sumar(num1, num2, num3);
         this.multiplicar(num1, num2, num3, num4, num5);
         this.restar(num1, num2);
         this.potencia();
         ModeloCalculadora divisioncalculadora = new ModeloCalculadora();
-        divisioncalculadora.setNumeroUno(10);
-        divisioncalculadora.setNumeroDos(5);
+        divisioncalculadora.setNumeroUno(num1);
+        divisioncalculadora.setNumeroDos(num2);
         this.division(divisioncalculadora);
     }
 }
--- Ejercicio3Poo/src/main/java/com/mycompany/ejercicio3poo/Main.java
+++ Ejercicio3Poo/src/main/java/com/mycompany/ejercicio3poo/Main.java
@@ -6,19 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
         
-        ModeloCalculadora calcular  = new ModeloCalculadora();
-        Calculadora resultados = new Calculadora();
-        int resultadosumas = resultados.sumar(2, 2, 2);
+        ModeloCalculadora modeloCalculadora  = new ModeloCalculadora();
+        Calculadora calculadora = new Calculadora();
+        int resultadosumas = calculadora.sumar(2, 2, 2);
         System.out.println("la sumas es : "+resultadosumas); //suma
-        String resultadoresta = resultados.restar(2, 1);
+        String resultadoresta = calculadora.restar(2, 1);
         System.out.println(resultadoresta);//resta
-        double resultadomultiplicacion = resultados.multiplicar(2, 2, 2, 2, 2);
+        double resultadomultiplicacion = calculadora.multiplicar(2, 2, 2, 2, 2);
         System.out.println("el resultado multiplicacion es : "+resultadomultiplicacion);//multiplicacion
-        calcular.setNumeroUno(10);
-        calcular.setNumeroDos(5);
-        float resultadodivison = resultados.division(calcular);
+        modeloCalculadora.setNumeroUno(10);
+        modeloCalculadora.setNumeroDos(5);
+        float resultadodivison = calculadora.division(modeloCalculadora);
         System.out.println("el resultaod de la division es :"+ resultadodivison);
-        String resultadopotencia = resultados.potencia();
+        String resultadopotencia = calculadora.potencia();
         System.out.println(resultadopotencia);
         Calculadora resultadosconstructores = new Calculadora(7,8,9,4,5);
     }
